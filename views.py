238aa1aa4b1fb80f442ffd514d52503ca911d7c4
Alexandros Stergiou <jutaroroche@gmail.com>
2020-12-03 22:46:43 +0100

    Ajout des requests dans le Flask

--- views.py
+++ views.py
@@ -3,8 +3,16 @@ from flask import *
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
+	if request.method=="POST":
+		city=request.form.get("city")
+		spot=request.form.get("spot")
+		name=request.form.get("name")
+		date=request.form.get("date")
+		times=request.form.get("times")
+		timee=request.form.get("timee")
+
 	return render_template("index.html")
 
 
